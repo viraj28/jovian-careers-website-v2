b7e7438f8a630fcb4c269c26f92d80e41ff3f45e
viraj28 <vnirbhavane28@gmail.com>
2023-06-12 15:13:58 +0000

    add css in style tag

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  return "Hello, Jovian"
+  return render_template("home.html")
 
 
 if __name__ == '__main__':
