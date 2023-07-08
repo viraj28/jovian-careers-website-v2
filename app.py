6468f9a0d3111d666a58d5dfac63d737d959047a
viraj28 <vnirbhavane28@gmail.com>
2023-07-08 16:11:14 +0000

    add dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -31,7 +31,7 @@ JOBS = [{
 @app.route("/")
 def hello():
   jobs = load_jobs_from_db()
-  return render_template("home.html", jobs=jobs, company_name="Jovian")
+  return render_template("home.html", jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -39,7 +39,13 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-  
+@app.route("/api/job/<id>")
+def list_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template("jobpage.html", job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -14,3 +14,12 @@ def load_jobs_from_db():
       jobs.append(dict(row))
     return jobs
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), dict(val=id))
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
